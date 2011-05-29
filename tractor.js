9591710f26e10528cec4f64359d4b8d15efb3c7d
Phil Cowans <phil@songkick.com>
2011-05-29 09:35:48 +0100

    Grab event calendar for each artist identified

--- tractor.js
+++ tractor.js
@@ -2,13 +2,51 @@ window.Artist = Backbone.Model.extend({});
 
 window.ArtistCollection = Backbone.Collection.extend({
   model: Artist,
+  
+  initialize: function() {
+    _.bindAll(this, "url");
+  },
+	
+  url: function() {
+    return "http://localhost:3000/textract?url=" + escape(this.currentTabUrl);
+  }
+});
+
+window.Event = Backbone.Model.extend({});
+
+window.Calendar = Backbone.Collection.extend({
+  model: Event,
 
   initialize: function() {
     _.bindAll(this, "url");
   },
 
   url: function() {
-    return "http://localhost:3000/textract?url=" + escape(this.currentTabUrl);
+    return "http://api.songkick.com/api/3.0/artists/mbid:" + escape(this.musicbrainzID) + "/calendar.json?apikey=musichackday";
+  },
+
+  parse: function(response) {
+    return response.resultsPage.results.event;
+  }
+});
+
+window.SongkickCalendarView = Backbone.View.extend({
+  initialize: function() {
+    _.bindAll(this, "setCalendar", "render");
+  },
+
+  render: function() {
+    var el = this.el;
+    this.calendar.each(function(event) {
+      var calendarListItem = $("<li>" + event.attributes.displayName  + "</li>")
+      el.append(calendarListItem);
+    });
+  },
+
+  setCalendar: function(calendar) {
+    this.calendar = calendar;
+    this.calendar.bind("refresh", this.render);
+    this.calendar.fetch();
   }
 });
 
@@ -20,6 +58,13 @@ window.ArtistView = Backbone.View.extend({
   
   render: function(artist) {
     $(this.el).html(this.template(artist.toJSON()));
+    
+    var calendar = new Calendar;
+    calendar.musicbrainzID = artist.attributes.mbid;
+    
+    var songkickCalendarView = new SongkickCalendarView;
+    songkickCalendarView.el = this.$('.calendar');
+    songkickCalendarView.setCalendar(calendar);
   }
 });
   
@@ -44,5 +89,5 @@ window.AppView = Backbone.View.extend({
       artistView.render(artist);
       el.append(artistListItem);
     });
-	}
+  }
 });
